8914b4159d4d9af7b4135fc6f114d1e3b2e09e51
Vlad Velichko <ya.super.vladik@gmail.com>
2016-02-20 15:07:15 +0300

    + category and achievement to Admin panel

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from post.models import Category, Achievement
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text',)
+    fields = ('text',)
+
+
+class AchievementAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title',)
+    fields = ('title', 'image_url')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Achievement, AchievementAdmin)
