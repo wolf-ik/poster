711225c1b6852f79c9aacf423dfe6fad60c7bc83
Vlad Velichko <ya.super.vladik@gmail.com>
2016-02-22 15:14:08 +0300

    working!

--- static/ckeditor/build-config.js
+++ static/ckeditor/build-config.js
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/7c6a2da5abb478d810a8d743746ad613
+ * (2) http://ckeditor.com/builder/5ff11983157173f2811f9165d139e152
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/7c6a2da5abb478d810a8d743746ad613
+ * (3) http://ckeditor.com/builder/download/5ff11983157173f2811f9165d139e152
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -50,12 +50,12 @@ var CKBUILDER_CONFIG = {
 		'tests'
 	],
 	plugins : {
-		'a11yhelp' : 1,
 		'basicstyles' : 1,
 		'blockquote' : 1,
-		'btbutton' : 1,
 		'clipboard' : 1,
 		'contextmenu' : 1,
+		'elementspath' : 1,
+		'enterkey' : 1,
 		'entities' : 1,
 		'filebrowser' : 1,
 		'floatingspace' : 1,
@@ -68,15 +68,13 @@ var CKBUILDER_CONFIG = {
 		'link' : 1,
 		'list' : 1,
 		'magicline' : 1,
-		'markdown' : 1,
-		'mathjax' : 1,
 		'maximize' : 1,
 		'pastefromword' : 1,
 		'pastetext' : 1,
 		'removeformat' : 1,
+		'resize' : 1,
 		'scayt' : 1,
 		'showborders' : 1,
-		'sourcearea' : 1,
 		'specialchar' : 1,
 		'stylescombo' : 1,
 		'tab' : 1,
